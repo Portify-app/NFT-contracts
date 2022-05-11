ce9243ed5b59a94ee85f284f2c144556ca924a51
Maga <steam.30mb1@gmail.com>
2022-05-11 10:35:05 +0300

    Deploy script

---
+++ deploy/00-deploy-nft.js
@@ -0,0 +1,17 @@
+const name = "PortifyNFT";
+const symbol = "PFY";
+const start = Math.floor(Date.now() / 1000) + 300;
+
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deployer} = await getNamedAccounts();
+
+    await deployments.deploy('PortifyNFT', {
+        from: deployer,
+        log: true,
+        args: [
+            name, symbol, start, deployer, ''
+        ]
+    });
+};
+module.exports.tags = ['nft'];
